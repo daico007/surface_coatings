b66b876ce2931aa31a22f94ce49e8a88e53b8616
Co Quach <43968221+daico007@users.noreply.github.com>
2022-03-04 14:48:41 -0600

    Generate bonds with freud (#7)
    * used freud_generate_bonds whenever available
    
    * remove print statements
    
    * add change methylstyrene to methylstyrene_initiator, exclusively use freud_generate_bonds
    
    * use freud_generate_bonds exclusively

--- surface_coatings/monomers/__init__.py
+++ surface_coatings/monomers/__init__.py
@@ -1,5 +1,5 @@
 from .azpma import AzPMA
 from .sbma import SBMA
 from .methacrylate import Methacrylate
-from .methylstyrene import MethylStyrene
+from .methylstyrene_initiator import MethylStyrene
 from .triazolebiotin import TriazoleBiotin
