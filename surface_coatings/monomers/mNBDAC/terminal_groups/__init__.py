b4a46781a05c362860f1c576b6f9707e88f40ad4
Co Quach <daico007@gmail.com>
2022-06-14 23:42:47 -0500

    add terminal groups for mNBDAC

--- surface_coatings/monomers/mNBDAC/terminal_groups/__init__.py
+++ surface_coatings/monomers/mNBDAC/terminal_groups/__init__.py
@@ -1,1 +1,7 @@
 from .acetaldehyde import Acetaldehyde
+from .acetic_acid import AceticAcid
+from .hydroxyl import Hydroxyl
+from .methyl import Methyl
+from .trifluoromethyl import Trifluoromethyl
+from .toluene import Toluene
+from .hexafluorobenzene import Hexafluorobenzene
