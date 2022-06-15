a172c0fc5efb78cbb91657db227c0b7321d2d225
nehilkieran@gmail.com <kieran@Admins-iMac-2.local>
2022-06-15 12:31:52 -0500

    Fixed import errors from uppercase directory imports

--- surface_coatings/__init__.py
+++ surface_coatings/__init__.py
@@ -1,2 +1,7 @@
 from .monolayer import Monolayer, DualMonolayer
 from .solvated_monolayer import SolvatedMonolayer, SolvatedDualMonolayer
+
+from .chains import *
+from .molecules import *
+from .monomers import *
+from .surfaces import *
