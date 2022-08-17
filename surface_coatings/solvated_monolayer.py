fe369813f1bbec5b838a55c3ce63574e0aefbffb
Co Quach <daico007@gmail.com>
2022-08-17 14:17:34 -0500

    better alignment of solvent and monolaer

--- surface_coatings/solvated_monolayer.py
+++ surface_coatings/solvated_monolayer.py
@@ -24,13 +24,22 @@ class SolvatedMonolayer(mb.Compound):
         super(SolvatedMonolayer, self).__init__()
         monolayer_box_lengths = monolayer.get_boundingbox().lengths
         surface_box_lengths = monolayer["tiled_surface"].get_boundingbox().lengths
+        # Calculate surface coords
+        xs, ys, zs = list(), list(), list()
+        for pos in monolayer["tiled_surface"].xyz:
+            xs.append(pos[0]), ys.append(pos[1]), zs.append(pos[2])
+        xs.sort(), ys.sort(), zs.sort()
+        surface_bounding_coords = ((xs[0], xs[-1]), (ys[0], ys[-1]), (zs[0], zs[-1]))
+
         solvent_box = [surface_box_lengths[0],
                        surface_box_lengths[1],
                        solvent_box_height]
         box_of_solvent = mb.fill_box(compound=solvent,
                                      box=solvent_box,
                                      n_compounds=n_solvents)
-        box_of_solvent.translate([0, 0, monolayer_box_lengths[2]])
+        box_of_solvent.translate([surface_bounding_coords[0][0],
+                                  surface_bounding_coords[1][0],
+                                  monolayer_box_lengths[2]])
         self.add(monolayer, label="monolayer")
         self.add(box_of_solvent, label="solvent")
 
@@ -53,17 +62,29 @@ class SolvatedDualMonolayer(mb.Compound):
         super(SolvatedDualMonolayer, self).__init__()
         top_monolayer = dual_monolayer["top_monolayer"]
         top_monolayer_box_lengths = top_monolayer.get_boundingbox().lengths
+
         bottom_monolayer = dual_monolayer["bottom_monolayer"]
         bottom_monolayer_box_lengths = bottom_monolayer.get_boundingbox().lengths
+
+        # Calculate surface coords
+        xs, ys, zs = list(), list(), list()
+        for pos in bottom_monolayer["tiled_surface"].xyz:
+            xs.append(pos[0]), ys.append(pos[1]), zs.append(pos[2])
+        xs.sort(), ys.sort(), zs.sort()
+        bottom_surface_coords = ((xs[0], xs[-1]), (ys[0], ys[-1]), (zs[0], zs[-1]))
+        bottom_surface_box_lengths = bottom_monolayer["tiled_surface"].get_boundingbox().lengths
+
         dual_monolayer_box_lengths = dual_monolayer.get_boundingbox().lengths
 
         separation = dual_monolayer_box_lengths[2] - (top_monolayer_box_lengths[2] + bottom_monolayer_box_lengths[2])
-        solvent_box = [bottom_monolayer_box_lengths[0],
-                       bottom_monolayer_box_lengths[1],
+        solvent_box = [bottom_surface_box_lengths[0],
+                       bottom_surface_box_lengths[1],
                        separation]
         box_of_solvent = mb.fill_box(compound=solvent,
                                      box=solvent_box,
                                      n_compounds=n_solvents)
-        box_of_solvent.translate([0, 0, bottom_monolayer_box_lengths[2]])
+        box_of_solvent.translate([bottom_surface_coords[0][0],
+                                  bottom_surface_coords[1][0],
+                                  bottom_monolayer_box_lengths[2]])
         self.add(dual_monolayer, label="monolayers")
         self.add(box_of_solvent, label="solvents")
