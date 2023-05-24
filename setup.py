d5f0e35249b2b88928d1a9b79c5225404bb88ee5
Co Quach <daico007@gmail.com>
2023-05-24 16:02:34 -0500

    opt out entry point for the moment

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from setuptools import setup
 setup(name="surface_coatings",
-      version="0.1.0", 
-      descriptions="Generic method to build coated surfaces", 
-      author="Co D. Quach", 
-      author_email="daico007@gmail.com", 
+      version="0.1.0",
+      descriptions="Generic method to build coated surfaces",
+      author="Co D. Quach",
+      author_email="daico007@gmail.com",
       license="MIT",
       packages=["surface_coatings"],
       zip_safe=False,
-      entry_points={
-            "mbuild.plugins": [
-                "Monolayer = surface_coatings.monolayer:Monolayer",
-                "DualMonolayer = surface_coatings.monolayer:DualMonolayer",
-                "SolvatedMonolayer = surface_coatings.solvated_monolayer:SolvatedMonolayer",
-                "SolvatedDualMonolayer = surface_coatings.solvated_monolayer:SolvatedDualMonolayer",
-            ]
-        }
+    # Temporarily turn off entry point setup
+    #   entry_points={
+    #         "mbuild.plugins": [
+    #             "Monolayer = surface_coatings.monolayer:Monolayer",
+    #             "DualMonolayer = surface_coatings.monolayer:DualMonolayer",
+    #             "SolvatedMonolayer = surface_coatings.solvated_monolayer:SolvatedMonolayer",
+    #             "SolvatedDualMonolayer = surface_coatings.solvated_monolayer:SolvatedDualMonolayer",
+    #         ]
+    #     }
     )
