7a12a398ecaf7e1d77e54227a42c5fefe65c7fb7
Co Quach <daico007@gmail.com>
2023-06-04 02:05:25 -0500

    add dimension for SilicaInterface class

--- surface_coatings/surfaces/silica_interface.py
+++ surface_coatings/surfaces/silica_interface.py
@@ -2,9 +2,9 @@ import mbuild as mb
 from mbuild.lib.surfaces import Betacristobalite
 
 class SilicaInterface(mb.Compound):
-    def __init__(self, tile_x=1, tile_y=1):
+    def __init__(self, dimensions=(5.388800 * 1.2, 4.589110 * 1.25, 1.3200 * 2), tile_x=1, tile_y=1):
         super(SilicaInterface, self).__init__()
-        silica_crystal = Betacristobalite()
+        silica_crystal = Betacristobalite(dimensions)
         tiled_compound = mb.lib.recipes.TiledCompound(mb.clone(silica_crystal),
                                                       n_tiles=(tile_x, tile_y, 1))
         self.add(tiled_compound, "TiledCompound")
